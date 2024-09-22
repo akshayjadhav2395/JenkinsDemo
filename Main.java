80652ff6c702cfb6a824813996f87f65d4d0c5aa
Akshay Jadhav <akshayjadhav4324@gmail.com>
2024-09-22 22:06:42 +0530

    TimeAndDate details
    Implemented function to give current date and time.

--- Main.java
+++ Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello World..");
         System.out.println("Learning Jenkins..!");
         System.out.println("Good Night...!");
+        System.out.println(LocalDateAndTime.now());
     }
 }
